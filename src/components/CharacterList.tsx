1677ffe0b4e086f4cb183991c8f9441db11f744e
OTakumi <takumi.o.0410@gmail.com>
2022-10-18 14:21:08 +0900

    Update CharacterList.tsx

--- src/components/CharacterList.tsx
+++ src/components/CharacterList.tsx
@@ -12,23 +12,19 @@ type Props = {
     characters: Character[]
 }
 
-const CharacterList: React.FC<Props> = (props) => {
-    const { school, characters } = props
-
-    return (
-        <div className="p-4">
-            <header className="text-lg">{school}</header>
-            <body>
-                {characters.map((character) => (
-                    <ul className="mb-2 list-disc">
-                        <li>{character.name}</li>
-                        <li>{character.grade}年生</li>
-                        <li>{character.height ? character.height : '???'}</li>
-                    </ul>
-                ))}
-            </body>
-        </div>
-    )
-}
+const CharacterList: React.FC<Props> = ({ school, characters }) => (
+    <div className="p-4">
+        <header className="text-lg">{school}</header>
+        <body>
+            {characters.map((character) => (
+                <ul className="mb-2 list-disc">
+                    <li>{character.name}</li>
+                    <li>{character.grade}年生</li>
+                    <li>{character.height ? character.height : '???'}</li>
+                </ul>
+            ))}
+        </body>
+    </div>
+)
 
 export default CharacterList
